dde8ed0ff9d33887fd6033d66954535285333a4e
AdrienLloyd <adrien_1996@hotmail.com>
2019-10-28 22:34:48 -0500

    ran a db:migrate

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,28 @@ FavouriteFood.destroy_all
 # if(created) ? skip it : create
 
 # testing git setup
+
+require 'open-uri'
+require 'json'
+require 'net/http'
+
+characters = JSON.parse(open('https://rickandmortyapi.com/api/character/').read)
+episodes = JSON.parse(open('https://rickandmortyapi.com/api/episode/').read)
+
+characters['results'].each do |character|
+  new_character = Character.create(
+    name: character['name'],
+    status: character['status'],
+    species: character['species'],
+    origin: character['origin']['name'],
+    image: character['image']
+  )
+  new_character.favouriteFood.create(
+    name: "#{Faker::Science.element} #{Faker::Food.dish}",
+    description: Faker::Food.description
+  )
+end
+
+puts "Generated #{Character.count} characters"
+puts "Generated #{FavouriteFood.count} characters"
+puts "Generated #{Episode.count} characters"
